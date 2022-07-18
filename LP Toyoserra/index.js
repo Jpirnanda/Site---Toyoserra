cad61898f4efeda3337a5bf77917310cd1835e08
Jay P <82546500+Jpirnanda@users.noreply.github.com>
2022-07-18 15:29:43 -0300

    Add files via upload

---
+++ LP Toyoserra/index.js
@@ -0,0 +1,7 @@
+var img_array = ['./Pressed.png', './Unpressed.png'];
+var delayInMilliseconds = 1000; //1 second
+
+
+function myFunction() {
+    document.getElementById("myImg").src = img_array[0];
+}
